415d8eb7c276384fee8aefe86b598879f063ae98
Mike Neilens <mike.neilens@gmail.com>
2019-11-27 14:22:14 +0000

    initial

---
+++ src/main/kotlin/BlogApp.kt
@@ -0,0 +1,111 @@
+package blog
+
+import com.auth0.jwt.*
+import com.auth0.jwt.algorithms.*
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.jackson.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.util.*
+
+data class Snippet(val user: String, val text: String)
+
+val snippets = Collections.synchronizedList(mutableListOf(
+    Snippet(user = "test", text = "hello"),
+    Snippet(user = "test", text = "world")
+))
+
+data class PostSnippet(val snippet: Text) {
+    data class Text(val text: String)
+}
+
+open class SimpleJWT(secret: String) {
+    private val algorithm = Algorithm.HMAC256(secret)
+    val verifier = JWT.require(algorithm).build()
+    fun sign(name: String): String = JWT.create().withClaim("name", name).sign(algorithm)
+}
+val simpleJwt = SimpleJWT("my-super-secret-for-jwt")
+
+class User(val name: String, val password: String)
+
+val users = Collections.synchronizedMap(
+    listOf(User("test", "test"))
+        .associateBy { it.name }
+        .toMutableMap()
+)
+class LoginRegister(val user: String, val password: String)
+
+class InvalidCredentialsException(message: String) : RuntimeException(message)
+
+fun Application.main() {
+
+    install(StatusPages) {
+        exception<InvalidCredentialsException> { exception ->
+            call.respond(HttpStatusCode.Unauthorized, mapOf("OK" to false, "error" to (exception.message ?: "")))
+        }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        allowCredentials = true
+        anyHost()
+    }
+
+    install(Authentication) {
+        jwt {
+            verifier(simpleJwt.verifier)
+            validate {
+                UserIdPrincipal(it.payload.getClaim("name").asString())
+            }
+        }
+    }
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog", ContentType.Text.Html)
+        }
+    }
+    install(ContentNegotiation) {
+        jackson {
+        }
+    }
+    routing {
+        post("/login-register") {
+            val post = call.receive<LoginRegister>()
+            val user = users.getOrPut(post.user) { User(post.user, post.password) }
+            if (user.password != post.password) throw InvalidCredentialsException("Invalid credentials")
+            call.respond(mapOf("token" to simpleJwt.sign(user.name)))
+        }
+
+        route("/snippets"){
+            get {
+                call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+            }
+            authenticate {
+                post {
+                    val post = call.receive<PostSnippet>()
+                    val principal = call.principal<UserIdPrincipal>() ?: error("No principal")
+                    snippets += Snippet(principal.name, post.snippet.text)
+                    call.respond(mapOf("OK" to true))
+                }
+                delete {
+                    call.receive<PostSnippet>()
+                    snippets.removeAll { true }
+                    call.respond(mapOf("OK - Deleted" to true))
+                }
+            }
+        }
+    }
+}
